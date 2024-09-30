a381bc18d4383cf4d29f68b2c20f594ed34ef2fe
Srikanth-vepuri <srikanthvepuri@lyrostech.com>
2024-09-30 13:25:43 +0530

    changes

--- src/Assessment-Project/AdminDashboard/AdminDashboard.js
+++ src/Assessment-Project/AdminDashboard/AdminDashboard.js
@@ -8,16 +8,17 @@ function AdminDashboardFunction() {
     const navigate = useNavigate();
 
     return (
-        <>
-            <div className="dashboard-container">
-                <div className="user-info">
+        <><div classname="home">
+            <div className="home-container">
+                <div className="home-info">
                     <p>Username: {globalUser.Username}</p>
                     <p>Email: {globalUser.Email}</p>
                 </div>
-                <div className="admin-navigation">
+                <div className="home-navigation">
                     <h2><Link to='/createQuiz' className="nav-link">Create a Quiz</Link></h2>
                     <h2><Link to='/viewResults' className="nav-link">View Results</Link></h2>
                 </div>
+            </div>
             </div>
         </>
     );
